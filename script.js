2e286e0b31e7a1d508fd99169ccc4d17a00a0624
PEDROHCA18 <114013463+PEDROHCA18@users.noreply.github.com>
2023-03-19 03:09:00 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const decisao = prompt('Quer namorar comigo? \nsim\nnão') 
+  if (decisao === 'sim') {
+    alert("Boa escolha, vou preparar as alianças")
+  } 
+  else {
+    alert('Errou, Responde denovo!!!')
+  }
